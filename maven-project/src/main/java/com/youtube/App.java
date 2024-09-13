c6a295522b5b0172cf2558041130b0ba7ce441a6
Priyanka K N <105920104+priyakn4844@users.noreply.github.com>
2024-09-13 12:00:20 +0530

    Update App.java

--- maven-project/src/main/java/com/youtube/App.java
+++ maven-project/src/main/java/com/youtube/App.java
@@ -6,5 +6,8 @@ package com.youtube;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("good morning !");
+        System.out.println("Have a nice day!");
+        
     }
 }
